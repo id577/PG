1d9d9b5cbe0147b873d4ca569a97bc27ff1046b4
id577 <83970636+id577@users.noreply.github.com>
2021-08-01 18:27:54 +0500

    Add files via upload

--- id577.sh
+++ id577.sh
@@ -856,7 +856,7 @@ fi
 
 temp=\$(OCLIF_TS_NODE=0 IRONFISH_DEBUG=1 ./run accounts:balance $IRONFISH_WALLET)
 
-balance=\$(echo \$temp | grep -Eo 'is: \\$IRON [0-9]+' | grep -Eo '[0-9]+')
+balance=\$(echo \$temp | grep -Eo 'is: \\\$IRON [0-9]+' | grep -Eo '[0-9]+')
 if [ "\$balance" = "" ]
 then
 	balance=0
