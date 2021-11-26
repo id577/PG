7140d5b1148ffff301fc452e43c87e4ad6b8ff20
id577 <83970636+id577@users.noreply.github.com>
2021-11-26 14:49:54 +0500

    Update id577.sh

--- id577.sh
+++ id577.sh
@@ -924,7 +924,7 @@ version=\$(echo \$temp | grep -Eo 'Version(:)* [0-9]*.[0-9]*.[0-9]*' | cut -d ' 
 temp=\$(OCLIF_TS_NODE=0 IRONFISH_DEBUG=1 ./run accounts:balance $IRONFISH_WALLET)
 
 balance=\$(echo \$temp | sed 's/,//g' | grep -Eo '\\\$ORE [0-9]+' | grep -Eo -m 1 '[0-9]+')
-if [ "$balance" = "" ]
+if [ "\$balance" = "" ]
 then
 	balance=0
 fi
