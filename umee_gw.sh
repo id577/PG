47dfc242db8bf6a8a12831c152661b9c4975d489
id577 <83970636+id577@users.noreply.github.com>
2022-01-08 17:29:17 +0500

    Add files via upload

--- umee_gw.sh
+++ umee_gw.sh
@@ -8,9 +8,17 @@ CONTRACT_ADDRESS="0xe54fbaecc50731afe54924c40dfd1274f718fe02"
 FEES=200
 FEES_BRIDGE=1
 API="1ZD4C1DWAI7AVZ73DNCGUUGFIV3RDPSKCG"
+ETH_PRICE=500
+UMEE_PRICE=1.4
+
+ORC_START_BALANCE=$(curl -s -X POST -d "module=account&action=balance&address=${ORC_WALLET}&tag=latest&apikey=${API}" https://api-goerli.etherscan.io/api | grep -Eo "result\":\"[0-9]+" | grep -Eo [0-9]+)
+ORC_START_BALANCE=$(bc<<<"scale=6;$ORC_START_BALANCE/1000000000000000000")
+
+ORC_UMEE_START_BALANCE=$(curl -s -X POST -d "module=account&action=tokenbalance&contractaddress=${CONTRACT_ADDRESS}&address=${ORC_WALLET}&tag=latest&apikey=${API}" https://api-goerli.etherscan.io/api | grep -Eo "result\":\"[0-9]+" | grep -Eo [0-9]+)
 
 echo -e "# UMEE GW v0.0.0"
 echo -e "# Choose mode:" 
+echo -e " 0) MODE 0 (Only Monitoring)"
 echo -e " 1) MODE 1 (send ONLY to ETH network)"
 echo -e " 2) MODE 2 (send ONLY to COSMOS network)"
 echo -e " 3) MODE 3 (send to ETH and COSMOS networks)"
@@ -44,11 +52,37 @@ function monitoring(){
 	ETH_BALANCE=$(curl -s -X POST -d "module=account&action=balance&address=${ETH_WALLET}&tag=latest&apikey=${API}" https://api-goerli.etherscan.io/api | grep -Eo "result\":\"[0-9]+" | grep -Eo [0-9]+)
 	ETH_BALANCE=$(bc<<<"scale=6;$ETH_BALANCE/1000000000000000000")
 	UMEE_AT_ETH_BALANCE=$(curl -s -X POST -d "module=account&action=tokenbalance&contractaddress=${CONTRACT_ADDRESS}&address=${ETH_WALLET}&tag=latest&apikey=${API}" https://api-goerli.etherscan.io/api | grep -Eo "result\":\"[0-9]+" | grep -Eo [0-9]+)
+  
+  ORC_BALANCE=$(curl -s -X POST -d "module=account&action=balance&address=${ORC_WALLET}&tag=latest&apikey=${API}" https://api-goerli.etherscan.io/api | grep -Eo "result\":\"[0-9]+" | grep -Eo [0-9]+)
+  ORC_BALANCE=$(bc<<<"scale=6;$ORC_BALANCE/1000000000000000000")
+  ORC_UMEE_BALANCE=$(curl -s -X POST -d "module=account&action=tokenbalance&contractaddress=${CONTRACT_ADDRESS}&address=${ORC_WALLET}&tag=latest&apikey=${API}" https://api-goerli.etherscan.io/api | grep -Eo "result\":\"[0-9]+" | grep -Eo [0-9]+)
+  ORC_UMEE_BALANCE=$(bc<<<"scale=6;$ORC_UMEE_BALANCE/1000000")
+  
+  LOSS=$(bc<<<"scale=6;$ORC_START_BALANCE-$ORC_BALANCE")
+  PROFIT=$(bc<<<"scale=2;$ORC_UMEE_BALANCE-$ORC_UMEE_START_BALANCE")
+ 
+  
+  LOSS_USD=$(bc<<<"scale=2;$LOSS*$ETH_PRICE")
+  PROFIT_USD=$(bc<<<"scale=2;$PROFIT*$UMEE_PRICE")
+  PNL=$(bc<<<"scale=2;$PROFIT-$LOSS")
+  
+	TX_PEGGO=$(curl -s localhost:1317/peggy/v1/module_state | grep ${UMEE_WALLET} | wc -l)
 	echo -e "------------ UMEE-GW MONITOR ------------"
 	echo -e "SETTINGS: MODE: ${MODE}; DT: ${DELAY_TIME}; CS: ${CYCLE_SHIFT}"
 	echo -e ""
-	echo -e "UMEE_WALLET: \e[32m${UMEE_BALANCE}\e[39muumee"
-	echo -e "ETH_WALLET: \e[32m${ETH_BALANCE}\e[39meth \e[32m${UMEE_AT_ETH_BALANCE}\e[39muumee"
+  echo -e "WALLETS STATS:"
+	echo -e "umee wallet: \e[32m${UMEE_BALANCE}\e[39muumee"
+	echo -e "ethereum wallet: \e[32m${ETH_BALANCE}\e[39meth \e[32m${UMEE_AT_ETH_BALANCE}\e[39muumee"
+  echo -e ""
+  echo -e "ORCHESTRATOR STATS:"
+  echo -e "balance: \e[32m${ORC_BALANCE}\e[39meth \e[32m${ORC_UMEE_BALANCE}\e[39muumee"
+  echo -e "profit: \e[32m${PROFIT}\e[39muumee (\e[32m${PROFIT_USD}\e[39m USD)"
+  echo -e "loss: \e[31m${LOSS}\e[39meth (\e[31m${LOSS_USD}\e[39m USD)"
+  if [[ $(bc -l <<< "${PNL}>0") -eq 1 ]]; then
+    echo -e "PnL: \e[32m${PNL}\e[39m USD"
+  else
+    echo -e "PnL: \e[31m${PNL}\e[39m USD"
+  fi
 	if [ "$MODE" = "1" ] || [ "$MODE" = "3" ]; then
     echo -e ""
 		echo -e "SUCCESSFUL TX_TO_ETH: ${SS_TX_TO_ETH}/${TX_TO_ETH}"
@@ -63,7 +97,9 @@ function monitoring(){
 		echo -e "ERRORS: \e[31m${ERR_TX_TO_COSMOS}\e[39m"
 	fi
 	echo -e ""
+  echo -e "TXs in PEGGO: ${TX_PEGGO}"
 	echo -e "-----------------------------------------"
+  echo -e ""
 	echo -e "Next update in ${DELAY_TIME} sec. Press ctrl+c to abort..."
 }
 
@@ -105,12 +141,12 @@ function exitd() {
 
 				#curl -s -X POST -d "module=account&action=tokentx&address=${ETH_WALLET}&startblock=0&endblock=999999999&sort=asc&apikey=${API}" https://api-goerli.etherscan.io/api | jq -r ".result[] | select(.to==\"$ETH_WALLET\") | .hash" >> ${TIMESTAMP}_TX_TO_ETH_HASHs.txt
 				
-				echo -e "COSMOS -> ETH:" >> {TIMESTAMP}_FULL_RESULT.txt
+				echo -e "COSMOS -> ETH:" >> ${TIMESTAMP}_FULL_RESULT.txt
 				IFS=' ' read -r -a HEIGHT_ARRAY <<< $(echo -e $(cat ${TIMESTAMP}_TXs_TO_ETH_LOG.txt | jq '.height'| sed 's/"//g') | sed 's/\n/ /g')
 				IFS=' ' read -r -a HASH_ARRAY <<< $(echo -e $(cat ${TIMESTAMP}_TXs_TO_ETH_LOG.txt | jq '.txhash'| sed 's/"//g') | sed 's/\n/ /g')
 				IFS=' ' read -r -a AMOUNT_ARRAY <<< $(echo -e $(cat ${TIMESTAMP}_TXs_TO_ETH_LOG.txt | jq '.logs[].events[] | select(.type=="transfer")' | jq '.attributes[] | select(.key=="amount")' | jq .value | sed 's/"//g') | sed 's/\n/ /g')
 				for (( n=0; n<$SS_TX_TO_ETH; n++ )); do
-					echo -e "${HEIGHT_ARRAY[n]} ${HASH_ARRAY[n]} ${AMOUNT_ARRAY[n]}" >> {TIMESTAMP}_FULL_RESULT.txt
+					echo -e "${HEIGHT_ARRAY[n]} ${HASH_ARRAY[n]} ${AMOUNT_ARRAY[n]}" >> ${TIMESTAMP}_FULL_RESULT.txt
 				done 
 			fi
 		fi
@@ -118,11 +154,11 @@ function exitd() {
 			if [ "$TX_TO_COSMOS" != "0" ]; then
 				cat ${TIMESTAMP}_TXs_TO_COSMOS_LOG.txt | grep -Eo "Transaction: [A-Za-z0-9]+" | awk '{print $2}' >> ${TIMESTAMP}_TX_TO_COSMOS_HASHs.txt
 	
-				echo -e "ETH -> COSMOS:" >> {TIMESTAMP}_FULL_RESULT.txt
+				echo -e "ETH -> COSMOS:" >> ${TIMESTAMP}_FULL_RESULT.txt
 				IFS=' ' read -r -a AMOUNT_ARRAY <<< $(echo -e $(cat ${TIMESTAMP}_TXs_TO_COSMOS_LOG.txt | grep -Eo "Amount: [0-9]+" | awk '{print $2}') | sed 's/\n/ /g')
 				IFS=' ' read -r -a HASH_ARRAY <<< $(echo -e $(cat ${TIMESTAMP}_TXs_TO_COSMOS_LOG.txt | grep -Eo "Transaction: [A-Za-z0-9]+" | awk '{print $2}') | sed 's/\n/ /g')
 				for (( n=0; n<$SS_TX_TO_COSMOS; n++ )); do
-					echo -e "${HASH_ARRAY[n]} ${AMOUNT_ARRAY[n]}" >> {TIMESTAMP}_FULL_RESULT.txt
+					echo -e "${HASH_ARRAY[n]} ${AMOUNT_ARRAY[n]}" >> ${TIMESTAMP}_FULL_RESULT.txt
 				done 
 			fi
 		fi
@@ -136,6 +172,14 @@ SS_TX_TO_ETH=0
 SS_TX_TO_COSMOS=0
 ERR_TX_TO_ETH=0
 ERR_TX_TO_COSMOS=0
+
+if [ "$MODE" = "0" ]; then
+  while true; do
+  monitoring
+  sleep $DELAY_TIME
+  done
+fi
+
 
 monitoring
 while true; do
@@ -160,4 +204,3 @@ if [ "$MODE" = "2" ] || [ "$MODE" = "3" ]; then
 	exitd
 fi
 done
-
